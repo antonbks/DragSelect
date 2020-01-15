32c20683ae4d9c2e14e1f83bac3c7ae841694705
Anton Buksa <anton.buksa@infomaniak.com>
2020-01-15 11:40:48 +0100

    add virtual scroll layer

--- src/DragSelect.js
+++ src/DragSelect.js
@@ -121,6 +121,7 @@ class DragSelect {
     this.moveCallback = onDragMove;
     this.callback = callback;
     this.area = this._handleArea(area);
+    this.area2 = document.getElementsByClassName('scrollable-content')[0]
     this.customStyles = customStyles;
     this.zoom = zoom;
 
@@ -128,6 +129,8 @@ class DragSelect {
     this.selector = selector || this._createSelector();
     this.selector.classList.add(this.selectorClass);
     this.start();
+    console.log("Hello world, from fork ;)")
+    console.log('area2', this.area2)
   }
 
   /**
@@ -281,8 +284,8 @@ class DragSelect {
    */
   start() {
     this._handleSelectables(this._initialSelectables);
-    this.area.addEventListener('mousedown', this._startUp);
-    this.area.addEventListener('touchstart', this._startUp, { passive: false });
+    this.area2.addEventListener('mousedown', this._startUp);
+    this.area2.addEventListener('touchstart', this._startUp, { passive: false });
   }
 
   /**
@@ -331,8 +334,8 @@ class DragSelect {
     this.area.removeEventListener('touchstart', this._startUp, {
       passive: false
     });
-    this.area.addEventListener('mousemove', this._handleMove);
-    this.area.addEventListener('touchmove', this._handleMove, {
+    this.area2.addEventListener('mousemove', this._handleMove);
+    this.area2.addEventListener('touchmove', this._handleMove, {
       passive: false
     });
     document.addEventListener('mouseup', this._end);
@@ -759,12 +762,12 @@ class DragSelect {
     this._previousCursorPos = this._getCursorPos(event, this.area);
     document.removeEventListener('mouseup', this._end);
     document.removeEventListener('touchend', this._end);
-    this.area.removeEventListener('mousemove', this._handleMove);
-    this.area.removeEventListener('touchmove', this._handleMove, {
+    this.area2.removeEventListener('mousemove', this._handleMove);
+    this.area2.removeEventListener('touchmove', this._handleMove, {
       passive: false
     });
-    this.area.addEventListener('mousedown', this._startUp);
-    this.area.addEventListener('touchstart', this._startUp, { passive: false });
+    this.area2.addEventListener('mousedown', this._startUp);
+    this.area2.addEventListener('touchstart', this._startUp, { passive: false });
 
     if (withCallback) this.callback(this.getSelection(), event);
     if (this._breaked) return false;
@@ -805,8 +808,8 @@ class DragSelect {
    */
   stop(remove = true, fromSelection = true, withCallback) {
     this.reset(false, withCallback);
-    this.area.removeEventListener('mousedown', this._startUp);
-    this.area.removeEventListener('touchstart', this._startUp, {
+    this.area2.removeEventListener('mousedown', this._startUp);
+    this.area2.removeEventListener('touchstart', this._startUp, {
       passive: false
     });
     document.removeEventListener('mouseup', this._end);
